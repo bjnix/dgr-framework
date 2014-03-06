e64a78b4588f60e69d7ee87980d04b51bae93d25
bjnix <bjnix@mtu.edu>
2014-03-05 20:39:12 -0500

    not completely tested!

--- sampleTypes.cpp
+++ sampleTypes.cpp
@@ -8,137 +8,154 @@
 #include <map>
 #include <typeinfo>
 #include <iostream>
+#include <sstream>
 #include <functional>
 
 #define BUFLEN 512
 
+enum type_enum      //enumeration of data 
+{ 
+    //
+    DGR_VOID,           //0 void
+    DGR_INT,            //1 int
+    DGR_DOUBLE,         //2 double
+    DGR_FLOAT,          //3 float
+    DGR_CHAR,           //4 char
+    DGR_BOOLEAN,        //5 boolean
+    //6->14 future use
+
+    //C arrays
+    DGR_PTR_VOID=16,    //16 void
+    DGR_PTR_INT,        //17 int
+    DGR_PTR_DOUBLE,     //18 double
+    DGR_PTR_FLOAT,      //19 float
+    DGR_PTR_CHAR,       //20 char
+    DGR_PTR_BOOLEAN,    //21 boolean
+    //16->30 future use
+    
+    //STL Containers
+    DGR_STD_STRING=32,  //32 std::string
+    //sequence containers
+    DGR_STD_ARRAY,      //33 std::array
+    DGR_STD_VECTOR,     //34 std::vector
+    DGR_STD_DEQUE,      //35 std::deque
+    DGR_STD_FORWARD_LIST,    //36 std::forward_list
+    DGR_STD_LIST,       //37 std::list
+
+    //container adaptors
+    DGR_STD_STACK,      //38 std::stack
+    DGR_STD_QUEUE,      //39 std::queue
+    DGR_STD_PRIORITY_QUEUE,   //40 std::priority_queue
+
+    //associative containers
+    DGR_STD_SET,        //41 std::set
+    DGR_STD_MULTISET,   //42 std::multiset
+    DGR_STD_MAP,        //43 std::map
+    DGR_STD_MULTIMAP,   //44 std::multimap
+
+    //Unordered associative containers
+    DGR_STD_UNORDERED_SET,        //45 std::unordered_set
+    DGR_STD_UNORDERED_MULTISET,   //46 std::unordered_multiset
+    DGR_STD_UNORDERED_MAP,        //47 std::unordered_map
+    DGR_STD_UNORDERED_MULTIMAP,   //48 std::unordered_multimap
+    //48->127 future use
+
+    //command section 128->256
+    CMD_NEW_OBJECT = 128
+                
+};
 
 /**
  * Name: MapNode
- * Description: A template class for a map node
+ * Description: A template class for a universal map node
  */
 class MapNodePtr{
 public:
-  std::string dataType;
-  std::string name;
+    std::string name;
+    type_enum dataType;
+    size_t dataLength;
+    virtual std::string getData(void) =0;
+    //setData callback function?
+    MapNodePtr(std::string n, type_enum dT, size_t dL) : name(n.c_str()), dataType(dT), dataLength(dL){}
 };
-typedef struct{
-  const char * name;
-}packetPtr;
 
+//only works with types that work with stringstream (native types)
 template<typename T>
 class MapNode : public MapNodePtr
 {
-public:
-  //MapNode(std::string n, T* d );
-  MapNode(std::string n, T* d );
-  MapNode(std::string n, T d);
-  
-  typedef struct packet: public packetPtr{
+protected:
     T data;
-  }packet;
 
-  packet packetData;
-  int packetLength;
-};
-
-template<typename T>
-MapNode<T>::MapNode(std::string n, T* d) {
-  name = n;
-  packetData.name = n.c_str();
-  packetData.data = *d;
-  dataType = typeid(*d).name();
-  packetLength = sizeof(packetData.name) + sizeof(packetData.data);
-};
-template<typename T>
-MapNode<T>::MapNode(std::string n, T d) {
-  name = n;
-  packetData.name = n.c_str();
-  packetData.data = d;
-  dataType = typeid(d).name();
-  std::cout<<sizeof(packetData)<<std::endl;
-  packetLength = sizeof(packetData);
-};
-std::vector<std::string> * serialize(std::map<std::string, MapNodePtr *> InputMap)
-{
-  char buf [BUFLEN];
-  int length = 0;
-  packetPtr * sendStruct;
-  std::vector<std::string> * packets = new std::vector<std::string>;
-  std::string message_buf;
-
-  for(auto it = InputMap.begin();it!= InputMap.end();it++)
-  {
-    
-
-    if(length >= (BUFLEN - 15))
-    {
-      return packets;
+public:
+    std::string getData(void){
+        std::stringstream ss;
+        ss << MapNode<T>::data; 
+        return ss.str;
     }
-    if(it->second->dataType == typeid(float).name()){ 
-      MapNode<float> * cur_node = (MapNode<float> *) it->second; 
-      sendStruct = &(cur_node->packetData);
-      length = cur_node->packetLength;
+    int setData(T d){ MapNode<T>::data = d; }
+    MapNode(std::string n, type_enum dT, size_t dL, T d) : MapNodePtr(n, dT, dL), data(d){}
       
-      message_buf.reserve( sizeof(cur_node->packetData) );
-      message_buf.append( (const char*) &(cur_node->packetData.name), sizeof(cur_node->packetData.name));
-      message_buf.append( (const char*) &(cur_node->packetData.data) , sizeof(cur_node->packetData.data));
-      std::cout << message_buf;
-    }
-    else if(it->second->dataType == typeid(double).name()){ 
-      MapNode<double> * cur_node = (MapNode<double> *) it->second; 
-      sendStruct = &(cur_node->packetData);
-      length = cur_node->packetLength;
-      std::string message_buf;
-      message_buf.reserve( sizeof(cur_node->packetData) );
-      message_buf.append( (const char*) &(cur_node->packetData.name), sizeof(cur_node->packetData.name));
-      message_buf.append( (const char*) &(cur_node->packetData.data) , sizeof(cur_node->packetData.data));
+};
 
-      std::cout << message_buf;
-    }
-    else if(it->second->dataType == typeid(int).name()){ 
-      MapNode<int> * cur_node = (MapNode<int> *) it->second; 
-      sendStruct = &(cur_node->packetData);
-      length = cur_node->packetLength;length = cur_node->packetLength;
-      std::string message_buf;
-      message_buf.reserve( sizeof(cur_node->packetData) );
-      message_buf.append( (const char*) &(cur_node->packetData.name), sizeof(cur_node->packetData.name));
-      message_buf.append( (const char*) &(cur_node->packetData.data) , sizeof(cur_node->packetData.data));
+std::string * serialize(std::map<std::string, MapNodePtr *> InputMap)
+{
+    std::string * packets = new std::string;
+    std::stringbuf message_buf;
+    int packet_counter = 0;
+    int node_length = 0;
+    int packet_length = 0;
+    bool first = true;
+    bool last = false;
 
-      std::cout << message_buf;    
-    }
-    else if(it->second->dataType == typeid(bool).name()){ 
-      MapNode<bool> * cur_node = (MapNode<bool> *) it->second; 
-      sendStruct = &(cur_node->packetData);
-      length = cur_node->packetLength;
-      std::string message_buf;
-      message_buf.reserve( sizeof(cur_node->packetData) );
-      message_buf.append( (const char*) &(cur_node->packetData.name), sizeof(cur_node->packetData.name));
-      message_buf.append( (const char*) &(cur_node->packetData.data) , sizeof(cur_node->packetData.data));
 
-      std::cout << message_buf;    
-    }
-    else if(it->second->dataType == typeid(std::string).name()){ 
-      MapNode<std::string> * cur_node = (MapNode<std::string> *) it->second; 
-      sendStruct = &(cur_node->packetData);
-      length = cur_node->packetLength;
-      std::string message_buf;
-      message_buf.reserve( sizeof(cur_node->packetData) );
-      message_buf.append( (const char*) &(cur_node->packetData.name), sizeof(cur_node->packetData.name));
-      message_buf.append( (const char*) &(cur_node->packetData.data) , sizeof(cur_node->packetData.data));
+    for(auto it = InputMap.begin();it!= InputMap.end();it++)
+    {
+       
+        if(std::next(it) == InputMap.end()) last = true;
+        
 
-      std::cout << message_buf;
+        
+
+        MapNodePtr * cur_node = it->second;
+        std::string cur_name = cur_node->name;
+        char cur_packet_num; 
+        char cur_type = cur_node->dataType;
+        std::string cur_data = cur_node->getData();
+        
+        if(first || last) cur_packet_num = (char)packet_counter;
+
+        node_length = cur_name.length() + sizeof(cur_type) + cur_data.length();
+
+        if(first || last) node_length += sizeof(cur_packet_num);
+
+        if(packet_length + node_length > BUFLEN)
+        {
+          std::cout << message_buf.str();
+          packet_length = 0;
+          first = false;
+
+        }else{
+        
+        if(first || last) { message_buf.sputc(cur_packet_num); node_length -= sizeof(cur_packet_num); }
+
+        message_buf.sputn(cur_node->name, cur_name.length()); 
+        message_buf.sputc(cur_node->dataType);
+        message_buf.sputn(cur_node->getData(), cur_data.length());
+
+        packet_length += node_length;
+
+        
+        first = false;
+        node_length = 0;
+        }
     }
-    else{ std::cout<<"error"<<std::endl; return packets;}
     
-    
-    
-  }
-  std::cout<<std::endl;
-  packets->push_back(message_buf);
-  return packets;
-}
-std::vector<std::string> * parser(std::vector<std::string> * packets){
+    std::cout << message_buf.str();
+    std::cout << std::endl;
+
+    return message_buf.str();
+    }
+std::vector<std::string> * parser(std::string * packets){
   std::string * pack;
   for(int i = 0; i < packets->size(); i++)
   {
@@ -168,7 +185,7 @@ std::vector<std::string> * parser(std::vector<std::string> * packets){
 //     {
 //       packet = split(*splitsIter, '`');
 //       InputMap.at(packet[0]) = (float)atof(packet[1].c_str());
-      
+
 //     }
 //   }
 // }
@@ -191,10 +208,10 @@ int main(){
     {node3->name,(MapNodePtr *)node3},
     {node4->name,(MapNodePtr *)node4},
     {node5->name,(MapNodePtr *)node5}
-  };
+};
 
   //std::cout << InputMap.at("first")->name <<" "<< ((MapNode<int>*) (InputMap.at("first")))->packetData.data << std::endl;
 parser(serialize(InputMap));
 
-  return 0;
+return 0;
 }
