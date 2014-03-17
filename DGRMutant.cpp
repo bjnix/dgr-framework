73c4ab26bcd480413e1bed2c857aa1b5f6e3d933
brentnix <drums5150@gmail.com>
2014-03-17 12:01:47 -0400

    degubbing

--- DGRMutant.cpp
+++ DGRMutant.cpp
@@ -24,7 +24,143 @@
 #define BUFLEN 512
 
 using namespace std;
+enum type_enum      //enumeration of data 
+{ 
+    //
+    DGR_VOID,           //0 void
+    DGR_INT,            //1 int
+    DGR_DOUBLE,         //2 double
+    DGR_FLOAT,          //3 float
+    DGR_CHAR,           //4 char
+    DGR_BOOL,           //5 bool
+    //6->14 future use
 
+    //C arrays
+    DGR_PTR_VOID=16,    //16 void *
+    DGR_PTR_INT,        //17 int *
+    DGR_PTR_DOUBLE,     //18 double *
+    DGR_PTR_FLOAT,      //19 float *
+    DGR_PTR_CHAR,       //20 char *
+    DGR_PTR_BOOL,       //21 bool *
+    //16->30 future use ---------------------------
+    
+    //STL Containers
+    DGR_STD_STRING=32,      //32 std::string
+
+    //sequence containers
+    DGR_STD_ARRAY,          //33 std::array
+    DGR_STD_VECTOR,         //34 std::vector
+    DGR_STD_DEQUE,          //35 std::deque
+    DGR_STD_FORWARD_LIST,   //36 std::forward_list
+    DGR_STD_LIST,           //37 std::list
+
+    //container adaptors
+    DGR_STD_STACK,          //38 std::stack
+    DGR_STD_QUEUE,          //39 std::queue
+    DGR_STD_PRIORITY_QUEUE, //40 std::priority_queue
+
+    //associative containers
+    DGR_STD_SET,            //41 std::set
+    DGR_STD_MULTISET,       //42 std::multiset
+    DGR_STD_MAP,            //43 std::map
+    DGR_STD_MULTIMAP,       //44 std::multimap
+
+    //Unordered associative containers
+    DGR_STD_UNORDERED_SET,      //45 std::unordered_set
+    DGR_STD_UNORDERED_MULTISET, //46 std::unordered_multiset
+    DGR_STD_UNORDERED_MAP,      //47 std::unordered_map
+    DGR_STD_UNORDERED_MULTIMAP, //48 std::unordered_multimap
+
+    //STL Container pointers
+    DGR_PTR_STD_STRING,         //50 std::string *
+
+    //sequence container pointers
+    DGR_PTR_STD_ARRAY,          //51 std::array *
+    DGR_PTR_STD_VECTOR,         //52 std::vector *
+    DGR_PTR_STD_DEQUE,          //53 std::deque *
+    DGR_PTR_STD_FORWARD_LIST,   //54 std::forward_list *
+    DGR_PTR_STD_LIST,           //55 std::list *
+
+    //container adaptor pointers
+    DGR_PTR_STD_STACK,          //56 std::stack *
+    DGR_PTR_STD_QUEUE,          //57 std::queue *
+    DGR_PTR_STD_PRIORITY_QUEUE, //58 std::priority_queue *
+
+    //associative container pointers
+    DGR_PTR_STD_SET,            //59 std::set *
+    DGR_PTR_STD_MULTISET,       //60 std::multiset *
+    DGR_PTR_STD_MAP,            //61 std::map *
+    DGR_PTR_STD_MULTIMAP,       //62 std::multimap *
+ 
+    //Unordered associative container pointers
+    DGR_PTR_STD_UNORDERED_SET,      //63 std::unordered_set *
+    DGR_PTR_STD_UNORDERED_MULTISET, //64 std::unordered_multiset *
+    DGR_PTR_STD_UNORDERED_MAP,      //65 std::unordered_map *
+    DGR_PTR_STD_UNORDERED_MULTIMAP, //66 std::unordered_multimap *
+    //67->127 future use ------------------------------------------------
+
+    //command section 128->256
+    DGR_PTR_FUNCTION = 128,          //128 function pointer for callbacks   
+    CMD_NEW_OBJECT
+                
+};
+template<typename T>
+type_enum typeid_int(T data_typeid)
+{
+    if(typeid(void)     == typeid(data_typeid)) return DGR_VOID;
+    else if(typeid(int)      == typeid(data_typeid)) return DGR_INT;
+    else if(typeid(bool)     == typeid(data_typeid)) return DGR_DOUBLE;
+    else if(typeid(float)    == typeid(data_typeid)) return DGR_FLOAT;
+    else if(typeid(char)     == typeid(data_typeid)) return DGR_CHAR;
+    else if(typeid(bool)     == typeid(data_typeid)) return DGR_BOOL;
+
+    else if(typeid(void *)   == typeid(data_typeid)) return DGR_PTR_VOID;
+    else if(typeid(int *)    == typeid(data_typeid)) return DGR_PTR_INT;
+    else if(typeid(bool *)   == typeid(data_typeid)) return DGR_PTR_DOUBLE;
+    else if(typeid(float *)  == typeid(data_typeid)) return DGR_PTR_FLOAT;
+    else if(typeid(char *)   == typeid(data_typeid)) return DGR_PTR_CHAR;
+    else if(typeid(bool *)   == typeid(data_typeid)) return DGR_PTR_BOOL;
+
+    else if(typeid(std::string)             == typeid(data_typeid)) return DGR_STD_STRING;
+    // if(typeid(std::array<U>)               == typeid(data_typeid)) return DGR_STD_ARRAY;
+    // if(typeid(std::vector)              == typeid(data_typeid)) return DGR_STD_VECTOR;
+    // if(typeid(std::deque))              == typeid(data_typeid)) return DGR_STD_DEQUE;
+    // if(typeid(std::forward_list)        == typeid(data_typeid)) return DGR_STD_FORWARD_LIST;
+    // if(typeid(std::list)                == typeid(data_typeid)) return DGR_STD_LIST;
+    // if(typeid(std::stack)               == typeid(data_typeid)) return DGR_STD_STACK;
+    // if(typeid(std::queue)               == typeid(data_typeid)) return DGR_STD_QUEUE;
+    // if(typeid(std::priority_queue)      == typeid(data_typeid)) return DGR_STD_PRIORITY_QUEUE;
+    // if(typeid(std::set)                 == typeid(data_typeid)) return DGR_STD_SET;
+    // if(typeid(set::multiset)            == typeid(data_typeid)) return DGR_STD_MULTISET;
+    // if(typeid(set::map)                 == typeid(data_typeid)) return DGR_STD_MAP;
+    // if(typeid(set::multimap)            == typeid(data_typeid)) return DGR_STD_MULTIMAP;
+    // if(typeid(set::unordered_set)       == typeid(data_typeid)) return DGR_STD_UNORDERED_SET;
+    // if(typeid(set::unordered_multiset)  == typeid(data_typeid)) return DGR_STD_UNORDERED_MULTISET;
+    // if(typeid(set::unordered_map)       == typeid(data_typeid)) return DGR_STD_UNORDERED_MAP;
+    // if(typeid(set::unordered_multimap)  == typeid(data_typeid)) return DGR_STD_UNORDERED_MULTIMAP;
+
+    // if(typeid(std::string *)            == typeid(data_typeid)) return DGR_PTR_STD_STRING;
+    // if(typeid(std::array *)             == typeid(data_typeid)) return DGR_PTR_STD_ARRAY;
+    // if(typeid(std::vector *)            == typeid(data_typeid)) return DGR_PTR_STD_VECTOR;
+    // if(typeid(std::deque *)             == typeid(data_typeid)) return DGR_PTR_STD_DEQUE;
+    // if(typeid(std::forward_list *)      == typeid(data_typeid)) return DGR_PTR_STD_FORWARD_LIST;
+    // if(typeid(std::list *)              == typeid(data_typeid)) return DGR_PTR_STD_LIST;
+    // if(typeid(std::stack *)             == typeid(data_typeid)) return DGR_PTR_STD_STACK;
+    // if(typeid(std::queue *)             == typeid(data_typeid)) return DGR_PTR_STD_QUEUE;
+    // if(typeid(std::priority_queue *)    == typeid(data_typeid)) return DGR_PTR_STD_PRIORITY_QUEUE;
+    // if(typeid(std::set *)               == typeid(data_typeid)) return DGR_PTR_STD_SET;
+    // if(typeid(set::multiset *)          == typeid(data_typeid)) return DGR_PTR_STD_MULTISET;
+    // if(typeid(set::map *)               == typeid(data_typeid)) return DGR_PTR_STD_MAP;
+    // if(typeid(set::multimap *)          == typeid(data_typeid)) return DGR_PTR_STD_MULTIMAP;
+    // if(typeid(set::unordered_set *)     == typeid(data_typeid)) return DGR_PTR_STD_UNORDERED_SET;
+    // if(typeid(set::unordered_multiset *)== typeid(data_typeid)) return DGR_PTR_STD_UNORDERED_MULTISET;
+    // if(typeid(set::unordered_map *)     == typeid(data_typeid)) return DGR_PTR_STD_UNORDERED_MAP;
+    // if(typeid(set::unordered_multimap *)== typeid(data_typeid)) return DGR_PTR_STD_UNORDERED_MULTIMAP;
+     else{
+        return DGR_VOID;
+     }
+
+}
 /**
  * Name: MapNode
  * Description: A template class for a map node
@@ -224,16 +360,12 @@ void sender() {
        
         node_length = 0;
         if(std::next(it) == InputMap.end()){ last_node = true;
-        std::cout << "last node!" << std::endl;}
-        if(first_node) std::cout<< "first node!" << std::endl;
         
         MapNodePtr * cur_node = it->second;
         std::string cur_name = cur_node->name;
         char* cur_data = cur_node->getDataString();
         int cur_data_length = cur_node->dataLength;
 
-        unsigned char cur_type = cur_node->dataType;
-        
         node_length = cur_name.length() + sizeof(char) + cur_data_length;
 
         // if(first_node || last_node) node_length += sizeof(packet_counter);
@@ -266,7 +398,6 @@ void sender() {
       
     }
     usleep(32000);
-  }
 }
 
 
